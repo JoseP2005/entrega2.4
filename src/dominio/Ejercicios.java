32fe933a21ad15125bb1257ca2ee9c2978b924a7
JoseP2005 <joseandres@usp.ceu.es>
2024-03-08 09:43:52 +0100

    0.1

---
+++ src/dominio/Ejercicios.java
@@ -0,0 +1,15 @@
+package dominio;
+
+public class Ejercicios {
+    //Ejercicio 34
+    /**
+     * Un algoritmo informático es un conjunto de instrucciones definidas, ordenadas y acotadas
+     * para resolver un problema, realizar un cálculo o desarrollar una tarea.
+     */
+    //Ejercicio 35
+    public static int ejercicio35 {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
